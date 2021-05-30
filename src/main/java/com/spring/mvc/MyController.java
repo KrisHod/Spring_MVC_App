475296eb8c2dfd7fe01e01bea9336ecc4b8f183e
Kris <krisgodom@gmail.com>
2021-05-30 11:39:55 -0300

    refactored adding @ModelAttribute

--- src/main/java/com/spring/mvc/MyController.java
+++ src/main/java/com/spring/mvc/MyController.java
@@ -2,12 +2,12 @@ package com.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -16,15 +16,13 @@ public class MyController {
     }
 
     @RequestMapping("/askDetails")
-    public String askEmpDetails(){
+    public String askEmpDetails(Model model) {
+        model.addAttribute("employee", new Employee());
         return "ask-emp-details-view";
     }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
-        
-        empName = "Mr. " + empName;
-        model.addAttribute("nameAttribute", empName);
+    public String showEmpDetails(@ModelAttribute("employee") Employee emp){
 
         return "show-emp-details-view";
     }
